f845aea74021453cacea8a874ad07c110680fd29
awnetcode <155958835+awnetcode@users.noreply.github.com>
2024-12-26 21:13:08 +0100

    priceList-component

--- src/components/main-content/Price-list.jsx
+++ src/components/main-content/Price-list.jsx
@@ -1,19 +1,27 @@
+import './styles/price-list.css';
 
 const PriceList = () =>{
     return(
         <>
-        <h1>Nasz cennik</h1>
-        <ul className="ammo-price-list">
-            <li className="ammo-price">.22 LR</li>
-            <li className="ammo-price">9x19</li>
-            <li className="ammo-price">.38 spec. / 7.62x25 / 7.56 Browning</li>
-            <li className="ammo-price">.45 ACP / .357 Magnum</li>
-            <li className="ammo-price">7.62x39</li>
-            <li className="ammo-price">.223 Rem / 5.56 NATO</li>
-            <li className="ammo-price">.308 Win / 30-06 Springfield</li>
-            <li className="ammo-price">12/70 Slug</li>
-        </ul>
-        
+        <h1>Nasz cennik:</h1>
+        <ul className="ammo-price-list">Za wystrzeloną amunicję:
+            <li className="ammo-price">.22 LR <span className="price">1 PLN</span></li>
+            <li className="ammo-price">9x19 <span className="price">2.50 PLN</span></li>
+            <li className="ammo-price">.38 spec. / 7.62x25 / 7.56 Browning <span className="price">4 PLN</span></li>
+            <li className="ammo-price">.45 ACP / .357 Magnum <span className="price">5 PLN</span></li>
+            <li className="ammo-price">7.62x39 <span className="price">5 PLN</span></li>
+            <li className="ammo-price">.223 Rem / 5.56 NATO <span className="price">5 PLN</span></li>
+            <li className="ammo-price">.308 Win / 30-06 Springfield <span className="price">9 PLN</span></li>
+            <li className="ammo-price">12/70 Slug <span className="price">8 PLN</span></li>
+            Za stanowisko:
+            <li className="ammo-price">30 min<span className="price">20 PLN</span></li>
+            <li className="ammo-price">60 min<span className="price">40 PLN</span></li>
+            <li className="ammo-price">2 godziny<span className="price">60 PLN</span></li>
+            <li className="ammo-price">3 godziny<span className="price">70 PLN</span></li>
+            <li className="ammo-price">Opieka instruktora<span className="price">20 PLN / godz.</span></li>
+            <li className="ammo-price">Cel reaktywny<span className="price">30 PLN / godz.</span></li>
+        </ul>  
+        <p>W przypadku strzelania z broni i amunicji obiektowej opieka instruktorska i opłata za wejście jest w cenie wystrzelonej amunicji.</p>
         </>
     )
 }
--- src/components/main-content/Shooting-range.jsx
+++ src/components/main-content/Shooting-range.jsx
@@ -2,7 +2,10 @@
 const ShootingRange = () =>{
     return(
         <>
-        Our range is AWESOME!
+        <h1>Strzelnica</h1>
+        <p>Nasz obiekt to profesjonalna strzelnica otwarta. Działamy od poniedziałku do soboty w godzinach 8:00 - 20:00. W niedziele organizujemy zawody sportowe i pikiniki kolekcjonerskie.</p>
+        <p>Dysponujemy pięcioma osiami strzeleckimi. Jedna do strzelań długodystansowych o długości 200m. Jedna 100m. Jedna 50m. Dwie osie taktyczno - dynamiczne 25m.</p>
+
         </>
     )
 }
