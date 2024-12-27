5baded84b0f80158bb601ecf999fb3c4c888b83e
awnetcode <155958835+awnetcode@users.noreply.github.com>
2024-12-27 10:22:32 +0100

    guns-data

---
+++ src/components/Guns-data.jsx
@@ -0,0 +1,45 @@
+const Guns = [
+    {
+        name: 'Glock 19',
+        caliber: '9x19 Para',
+        image: '../images/glock 19.jpg'
+    },
+    {
+        name: 'Colt 1911',
+        caliber: '.45 ACP',
+        image: '../images/1911.jpg'
+    },
+    {
+        name: 'AK',
+        caliber: '7.62x39',
+        image: '../images/AK with scope.jpg'
+    },
+    {
+        name: 'GSG 1911',
+        caliber: '.22 LR',
+        image: '../images/GSG 1911.avif'
+    },
+    {
+        name: 'I dużo więcej',
+        caliber: 'Wiele różnych',
+        image: '../images/handguns table.jpg'
+    },
+    {
+        name: 'Rossi Puma',
+        caliber: '40/70 Gov.',
+        image: '../images/rossi Lever Action.jpg'
+    },
+    {
+        name: 'Colt Single Action Army',
+        caliber: '.357 Magnum',
+        image: '../images/SAA revolver.jpg'
+    },
+    {
+        name: 'AR - 15 Anderson',
+        caliber: '.223 Rem',
+        image: '../images/SBR AR.avif'
+    },
+]
+
+export default Guns;
+
--- src/components/main-content/Our-guns.jsx
+++ src/components/main-content/Our-guns.jsx
@@ -1,10 +1,20 @@
+import './styles/our-guns.css';
+import Guns from '../Guns-data';
 
 const OurGuns = () =>{
+
+    {Guns.map((gun, index) => {
+        console.log(gun);
+        console.log(index);
+        gun.name;
+    })}
     return(
         <>
         Guns are Awesome!
+        <img src={Guns[3].image} alt="" />
         </>
     )
+
 }
 
 export default OurGuns;
